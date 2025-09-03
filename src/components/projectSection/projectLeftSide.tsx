3e9c4c0179fbfbdbaef7b087fb00786bd1d815c6
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-03 15:09:56 -0300

    refactor: refina para um código mais limpo e ajusta responsividade

--- src/components/projectSection/projectLeftSide.tsx
+++ src/components/projectSection/projectLeftSide.tsx
@@ -17,9 +17,9 @@ export default function ProjectLeftSide({
 }) {
   return (
     <Link href={link} target="blank" rel="noopener noreferrer">
-      <section className="flex items-center gap-4 p-6 flex-col text-gray-200 bg-gray-700 rounded-lg md:flex-row md:hover:bg-gray-800 transition-colors duration-700">
+      <section className="flex flex-col items-center gap-4 p-6 rounded-xl border-2 border-gray-500 text-gray-200 bg-gray-700 lg:flex-row md:hover:bg-gray-800 transition-colors duration-700">
         <Image
-          className="rounded-lg duration-700 hover:scale-105 hover:duration-300 hover:ease-in hover:bg-orange-gradient hover:shadow-xl hover:shadow-orange-700"
+          className="duration-700 hover:scale-105 hover:duration-300 hover:ease-in hover:shadow-lg hover:shadow-orange"
           src={imgPath}
           alt="Projeto do Portfolio"
           width={600}
@@ -27,11 +27,11 @@ export default function ProjectLeftSide({
         />
 
         <article className="md:w-2/3 rounded-3xl px-6 gap-8 flex flex-col justify-center">
-          <h3 className="text-3xl font-bold bg-text-blue-gradient bg-clip-text text-transparent">
+          <h3 className="text-2xl md:text-3xl font-bold bg-text-dark-blue-gradient bg-clip-text text-transparent">
             {title}
           </h3>
           <p className="text-sm md:text-base">{description}</p>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 text-sm md:text-base">
             <span>Desenvolvido com:</span>
             {icons.map((Icon, index) => (
               <Icon key={index} size={25} />
--- src/components/projectSection/projectRightSide.tsx
+++ src/components/projectSection/projectRightSide.tsx
@@ -16,40 +16,29 @@ export default function ProjectRightSide({
   icons: IconType[];
 }) {
   return (
-    <section className="flex gap-4 py-6 flex-col md:flex-row">
-      <article className="hidden md:flex md:w-2/3 rounded-3xl px-6 gap-8 flex-col justify-center">
-        <h3 className="text-2xl font-bold bg-text-dark-gradient bg-clip-text text-transparent">
-          {title}
-        </h3>
-        <p>{description}</p>
-        <div className="flex items-center gap-2">
-          <span>Desenvolvido com:</span>
-          {icons.map((Icon, index) => (
-            <Icon key={index} size={25} />
-          ))}
-        </div>
-      </article>
-      <Link href={link} target="blank" rel="noopener noreferrer">
+    <Link href={link} target="blank" rel="noopener noreferrer">
+      <section className="flex items-center gap-4 p-6 flex-col border-2 border-zinc-500 text-gray-200 bg-zinc-700 rounded-xl lg:flex-row md:hover:bg-zinc-800 transition-colors duration-700">
+        <article className="flex order-2 lg:order-1 md:w-2/3 rounded-3xl px-6 gap-8 flex-col justify-center">
+          <h3 className="text-2xl md:text-3xl font-bold bg-text-dark-gradient bg-clip-text text-transparent">
+            {title}
+          </h3>
+          <p className="text-sm md:text-base">{description}</p>
+          <div className="flex items-center gap-2 text-sm md:text-base">
+            <span>Desenvolvido com:</span>
+            {icons.map((Icon, index) => (
+              <Icon key={index} size={25} />
+            ))}
+          </div>
+        </article>
+
         <Image
-          className="rounded-lg duration-700 hover:scale-105 hover:duration-300 hover:ease-in hover:bg-orange-gradient hover:shadow-xl hover:shadow-orange-700"
+          className="order-1 lg:order-2 duration-700 hover:scale-105 hover:duration-300 hover:ease-in hover:shadow-lg hover:shadow-orange"
           src={imgPath}
           alt="Projeto do Portfolio"
           width={600}
           height={600}
         />
-      </Link>
-      <div className="md:hidden rounded-3xl p-6 gap-8 flex flex-col justify-center">
-        <h3 className="text-2xl font-bold bg-text-dark-gradient bg-clip-text text-transparent">
-          {title}
-        </h3>
-        <p className="text-sm">{description}</p>
-        <div className="flex items-center gap-2">
-          <span>Desenvolvido com:</span>
-          {icons.map((Icon, index) => (
-            <Icon key={index} size={25} />
-          ))}
-        </div>
-      </div>
-    </section>
+      </section>
+    </Link>
   );
 }
