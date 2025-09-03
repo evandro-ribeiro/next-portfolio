ac1c49417c408db8797b72300eae78dff74f4442
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-03 15:10:58 -0300

    style: ajusta responsividade para mobile e ajusta espaçamento

--- src/components/stacks.tsx
+++ src/components/stacks.tsx
@@ -11,7 +11,7 @@ export default function Stack({
 }) {
   return (
     <li
-      className={`text-xs md:text-sm backdrop-blur-lg flex font-semibold gap-2 items-center p-1 md:p-4 rounded-full shadow-md lg:shadow-white lg:text-gray-100 duration-200 hover:scale-110 ${colorItem}`}
+      className={`text-xs md:text-sm backdrop-blur-lg flex font-semibold gap-2 items-center p-2 md:p-4 rounded-full shadow-sm md:shadow-md md:shadow-white md:text-gray-100 duration-200 hover:scale-110 ${colorItem}`}
     >
       <Icon className="md:size-7 size-5 p-0.5" />
       {title}
