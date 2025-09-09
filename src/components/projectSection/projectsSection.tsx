59eaefcb7644d945313f25392c0c4b2d0ad7f8fb
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-09 11:48:50 -0300

    feat: adiciona projeto noknok e ferline ao portfolio

--- src/components/projectSection/projectsSection.tsx
+++ src/components/projectSection/projectsSection.tsx
@@ -1,4 +1,4 @@
-import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
+import { FaCss3Alt, FaHtml5, FaReact, FaWordpress } from "react-icons/fa";
 import { SiJavascript, SiNextdotjs, SiTailwindcss } from "react-icons/si";
 import ProjectLeftSide from "./projectLeftSide";
 import ProjectRightSide from "./projectRightSide";
@@ -17,8 +17,28 @@ export default function ProjectsSection() {
 
       <section className="flex flex-col gap-12 md:pb-20 md:mx-32 mx-10">
         <ProjectLeftSide
+          link={"https://ferline.co"}
+          imgPath={"/ferline.png"}
+          title={"Ferline Company"}
+          description={
+            "Landing Page de Gestor de Tráfego Pago criada para apresentar a empresa e seus serviços, com o objetivo de atrair novos clientes e gerar leads."
+          }
+          icons={[FaWordpress]}
+        />
+
+        <ProjectRightSide
+          link={"https://noknokdesign.com.br/"}
+          imgPath={"/noknok.png"}
+          title={"NokNok Design"}
+          description={
+            "Site de móveis com design minimalista, projetado para demonstrar os serviços prestados pela marca e divulgar suas informações."
+          }
+          icons={[FaWordpress]}
+        />
+
+        <ProjectLeftSide
           link={"https://meva-consultoria.vercel.app/"}
-          imgPath={"/project1.png"}
+          imgPath={"/meva.png"}
           title={"Meva Consultoria"}
           description={
             "Site de consultoria cervejeira, projetado para demonstrar os serviços prestados pela marca e divulgar suas informações."
@@ -28,7 +48,7 @@ export default function ProjectsSection() {
 
         <ProjectRightSide
           link={"https://next-casaverde.vercel.app/"}
-          imgPath={"/project2.png"}
+          imgPath={"/casaverde.png"}
           title={"CasaVerde"}
           description={
             "Site de decorações para a casa, principalmente focado na venda de plantas. Projetado e desenvolvido como SPA para demonstrar o fluxo de páginas de produtos, página de venda e página de finalização de pedido."
@@ -38,7 +58,7 @@ export default function ProjectsSection() {
 
         <ProjectLeftSide
           link={"https://petshop-compass.vercel.app/"}
-          imgPath={"/project4.png"}
+          imgPath={"/petshop.png"}
           title={"PetShop Paws n' Play"}
           description={
             "Projeto minimalista desenvolvido com foco em tamanhos de tela específicos (Desktop para 1024px e 1440px, mobile para 390px), conforme critérios de aceite"
@@ -48,7 +68,7 @@ export default function ProjectsSection() {
 
         <ProjectRightSide
           link={"https://evandro-ribeiro.github.io/js-monks-challenge/"}
-          imgPath={"/project3.png"}
+          imgPath={"/monks.png"}
           title={"Monks Web"}
           description={
             "Landing page desenvolvida com foco em utilização de menor quantidade de bibliotecas para melhorar performance e simplicidade do projeto"
