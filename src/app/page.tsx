8400342c7a39afb2f219545d81b34ea3ea3457db
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-10 10:15:07 -0300

    feat: adiciona indexação do domínio na vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,26 @@ import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import InitialBanner from "@/components/initialBanner";
 import ProjectsSection from "@/components/projectSection/projectsSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="font-montserrat bg-gray-900 text-gray-200">
+      <Head>
+        <title>Evandro Machado - Desenvolvedor Full Stack</title>
+        <meta
+          name="description"
+          content="Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras."
+        />
+        <meta
+          property="og:title"
+          content="Evandro Machado - Desenvolvedor Full Stack"
+        />
+        <meta
+          property="og:description"
+          content="Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras."
+        />
+      </Head>
       <InitialBanner />
       <ProjectsSection />
       <CertificationSection />
