b6fd63fdc6d057c0a3159f5dc14fb5947c7d9068
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-16 20:18:06 -0300

    feat: adiciona ferramenta da vercel para acompanhar performance do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Portfolio | Evandro Machado",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
