5058a4f70674ef2533e9a110750113000313ed3e
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-26 19:06:04 -0300

    fix: otimização da imagem do hero para melhorar desempenho do site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config = {
         "linear-gradient(to bottom, #ffffff, #8f8fff, #3d00de)",
       "text-dark-gradient": "linear-gradient(to bottom, #ffffff, #002610)",
 
-      "bg-principal": "url('/bg-principal.png')",
+      "bg-principal": "url('/bg-principal.webp')",
     },
     fontFamily: {
       montserrat: ["Montserrat", "sans-serif"],
