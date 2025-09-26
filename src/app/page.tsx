933be8be7220693122c3820ea63ce0350b0a0a1c
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-09-26 19:06:44 -0300

    fix: ajusta a fonte dinamicamente no código, sem fazer requisição posterior.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,33 @@ import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import InitialBanner from "@/components/initialBanner";
 import ProjectsSection from "@/components/projectSection/projectsSection";
-import Head from "next/head";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Evandro Machado - Desenvolvedor Full Stack",
+  description:
+    "Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras.",
+  openGraph: {
+    title: "Evandro Machado - Desenvolvedor Full Stack",
+    description:
+      "Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras.",
+  },
+  keywords: [
+    "Desenvolvimento Web",
+    "Criação de sites",
+    "Criação de landing page",
+  ],
+};
+
+const montserrat = Montserrat({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <main className="font-montserrat bg-gray-900 text-gray-200">
-      <Head>
-        <title>Evandro Machado - Desenvolvedor Full Stack</title>
-        <meta
-          name="description"
-          content="Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras."
-        />
-        <meta
-          property="og:title"
-          content="Evandro Machado - Desenvolvedor Full Stack"
-        />
-        <meta
-          property="og:description"
-          content="Conheça o portfólio de Evandro Machado, um desenvolvedor full stack apaixonado por criar soluções inovadoras."
-        />
-      </Head>
+    <main className={`${montserrat.className} bg-gray-900 text-gray-200`}>
       <InitialBanner />
       <ProjectsSection />
       <CertificationSection />
