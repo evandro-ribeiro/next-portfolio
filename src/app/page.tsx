4fab4b6bb72894e7fb84f968e4ddd9ea46c6072b
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-11-06 16:09:37 -0300

    refactor: retira componentes não mais utilizados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import AdvantagesSection from "@/components/advantageSection/advantagesSection";
-import FrequentAskSection from "@/components/askSection/askSection";
 import CertificationSection from "@/components/certifications/certificationSection";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
@@ -35,8 +33,6 @@ export default function Home() {
       <InitialBanner />
       <ProjectsSection />
       <CertificationSection />
-      <AdvantagesSection />
-      <FrequentAskSection />
       <Contact />
       <Footer />
     </main>
